063edd71ab649a685aafe7b580c5fce51d08ef53
Nishantbhangale <60780073+NishantBhangale@users.noreply.github.com>
2026-06-24 00:35:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# this is python flask code for flash app, need python
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
